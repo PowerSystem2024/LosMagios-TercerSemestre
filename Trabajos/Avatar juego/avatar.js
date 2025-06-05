df1793463dcb3d3a3d8b27edded5c5aa44ab5190
ErwiinS2 <yamilcitadino@gmail.com>
2025-06-04 21:09:28 -0300

    Update avatar.js
    Tercera parte del codigo de avatar 04/06/2025

--- Trabajos/Avatar juego/avatar.js
+++ Trabajos/Avatar juego/avatar.js
@@ -1,6 +1,17 @@
+let ataqueJugador
+let ataqueEnemigo
+
 function iniciarJuego() {
     let botonPersonajeJugador = document.getElementById("boton-personaje");
     botonPersonajeJugador.addEventListener("click", seleccionarPersonajeJugador);
+
+    let botonPunio = document.getElementById('boton-punio')
+    botonPunio.addEventListener('click', ataquePunio)
+    let botonPatada = document.getElementById('boton-patada')
+    botonPatada.addEventListener('click', ataquePatada)
+    let botonBarrida = document.getElementById('boton-barrida')
+    botonBarrida.addEventListener('click', ataqueBarrida)
+
 }
 
 function seleccionarPersonajeJugador() {
@@ -34,13 +45,72 @@ function seleccionarPersonajeJugador() {
 }
 
 function seleccionarPersonajeEnemigo(personajeJugador) {
-    const personajes = ["Zuko", "Katara", "Aang", "Toph"];
-    const personajesDisponibles = personajes.filter(p => p !== personajeJugador);
-    const numeroAleatorio = Math.floor(Math.random() * personajesDisponibles.length);
-    const personajePC = personajesDisponibles[numeroAleatorio];
+    let personajeAleatorio = aleatorio(1, 4)
+    let spanPersonajeEnemigo = document.getElementById('personaje-enemigo')
 
-    let spanEnemigo = document.getElementById("personaje-enemigo");
-    spanEnemigo.innerHTML = personajePC;
+    if(personajeAleatorio == 1){
+        spanPersonajeEnemigo.innerHTML = 'Zuko'
+    } else if(personajeAleatorio == 2){
+        spanPersonajeEnemigo.innerHTML = 'Katara'
+    } else if(personajeAleatorio == 3){
+        spanPersonajeEnemigo.innerHTML = 'Aang'
+    } else {
+        spanPersonajeEnemigo.innerHTML = 'Toph'
+    }
+}
+
+function ataquePunio(){
+    ataqueJugador = 'Punio'
+    ataqueAleatorioEnemigo()
+    combate()
+}
+
+function ataquePatada(){
+    ataqueJugador = 'Patada'
+    ataqueAleatorioEnemigo()
+    combate()
+}
+
+function ataqueBarrida(){
+    ataqueJugador = 'Barrida'
+    ataqueAleatorioEnemigo()
+    combate()
+}
+
+function ataqueAleatorioEnemigo(){
+    let ataqueAleatorio = aleatorio(1, 3)
+
+    if(ataqueAleatorio == 1){
+        ataqueEnemigo = 'Punio'
+    } else if(ataqueAleatorio == 2){
+        ataqueEnemigo = 'Patada'
+    } else{
+        ataqueEnemigo = 'Barrida'
+    }
+}
+
+function combate() {
+    let resultado = "";
+    
+    if (ataqueJugador === ataqueEnemigo) {
+        resultado = "Empate";
+    } else if (
+        (ataqueJugador === "Punio" && ataqueEnemigo === "Barrida") ||
+        (ataqueJugador === "Barrida" && ataqueEnemigo === "Patada") ||
+        (ataqueJugador === "Patada" && ataqueEnemigo === "Punio")
+    ) {
+        resultado = "Ganaste esta ronda!";
+    } else {
+        resultado = "Perdiste esta ronda!";
+    }
+
+    alert("Tu ataque: " + ataqueJugador + "\nAtaque enemigo: " + ataqueEnemigo + "\n" + resultado);
+}
+
+
+
+function aleatorio(min, max){
+    return Math.floor( Math.random() * (max - min +1) + min)
 }
 
 window.addEventListener("load", iniciarJuego);
